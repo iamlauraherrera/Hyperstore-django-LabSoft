295940ae8650d942a7c0a83479ef64b37e55537a
Laura Ache <l.herrera1@utp.edu.co>
2023-12-01 03:46:56 -0500

    heroku server

--- Proyecto/tienda/tienda/settings.py
+++ Proyecto/tienda/tienda/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -48,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleare.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'tienda.urls'
@@ -143,5 +144,3 @@ os.makedirs(STATIC_ROOT, exist_ok=True)
 STATICFILES_DIRS = ( 
     os.path.join(BASE_DIR, 'static'),
 )
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\brayan\Desktop\LabSfot-master\LabSfot\venv
+set VIRTUAL_ENV=C:\Users\brayan\Desktop\Hyperstore-django-LabSoft\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
